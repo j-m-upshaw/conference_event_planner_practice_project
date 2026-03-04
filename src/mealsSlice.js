90be3d1a9c1e382e56f0c7b5c169e488a1b5d067
j-m-upshaw <jalandmupshaw@gmail.com>
2026-03-04 14:16:34 -0600

    Started the meal sections by starting an array

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,7 +4,29 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    {
+      name: "Breakfast",
+      cost: 50,
+      selected: false 
+    },
+
+    {
+      name: "Breakfast",
+      cost: 50,
+      selected: false 
+    },
+
+    {
+      name: "Breakfast",
+      cost: 50,
+      selected: false 
+    },
+
+    {
+      name: "Breakfast",
+      cost: 50,
+      selected: false 
+    }
   ],
   reducers: {
     toggleMealSelection: (state, action) => {

6685f7b5b5b0b108980ec56b75074a756996198b
j-m-upshaw <jalandmupshaw@gmail.com>
2026-03-04 13:27:07 -0600

    Added code for the avSlice and imported to the store.js

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,58 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
+
+    {
+      img: "https://pixabay.com/images/download/jarmoluk-projector-428664_1920.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0, 
+    },
+
+    {
+      img: "https://pixabay.com/images/download/bru-no-multimedia-870737_1920.jpg",
+      name: "Speaker",
+      cost: 35,
+      quantity: 0, 
+    },
+
+    {
+      img: "https://pixabay.com/images/download/spoiu23-microphone-5594702_1920.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0, 
+    },
+
+    {
+      img: "https://pixabay.com/images/download/clickerhappy-board-2209239_1920.jpg",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0, 
+    },
+
+    {
+      img: "https://pixabay.com/images/download/publicdomainpictures-signage-2653_1920.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0, 
+    }
     
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
+      const item = state[action.payload];
+      if (item) {
+        item.quantity++;
+      }
       
     },
     decrementAvQuantity: (state, action) => {
-     
+      const item = state[action.payload];
+      if (item && item.quantity > 0) {
+        item.quantity--;
+      }
     },
   },
 });

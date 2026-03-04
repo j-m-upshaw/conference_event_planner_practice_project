f12c7e94f0a2976f6d8572abfd378a41a921320c
j-m-upshaw <jalandmupshaw@gmail.com>
2026-03-04 14:08:53 -0600

    Fixed img urls for the add ons section

--- src/avSlice.js
+++ src/avSlice.js
@@ -5,35 +5,35 @@ export const avSlice = createSlice({
   initialState: [
 
     {
-      img: "https://pixabay.com/images/download/jarmoluk-projector-428664_1920.jpg",
+      img: "https://media.istockphoto.com/id/1429966133/photo/projector-on-the-ceiling.jpg?s=1024x1024&w=is&k=20&c=Eb-0wrjrxwbwQJAy9Any0wd9X1GNl-_UYNjON9JbO4c=",
       name: "Projectors",
       cost: 200,
       quantity: 0, 
     },
 
     {
-      img: "https://pixabay.com/images/download/bru-no-multimedia-870737_1920.jpg",
+      img: "https://images.pexels.com/photos/9842750/pexels-photo-9842750.jpeg",
       name: "Speaker",
       cost: 35,
       quantity: 0, 
     },
 
     {
-      img: "https://pixabay.com/images/download/spoiu23-microphone-5594702_1920.jpg",
+      img: "https://images.pexels.com/photos/8132463/pexels-photo-8132463.jpeg",
       name: "Microphones",
       cost: 45,
       quantity: 0, 
     },
 
     {
-      img: "https://pixabay.com/images/download/clickerhappy-board-2209239_1920.jpg",
+      img: "https://images.pexels.com/photos/5905445/pexels-photo-5905445.jpeg",
       name: "Whiteboards",
       cost: 80,
       quantity: 0, 
     },
 
     {
-      img: "https://pixabay.com/images/download/publicdomainpictures-signage-2653_1920.jpg",
+      img: "https://images.pexels.com/photos/1706018/pexels-photo-1706018.jpeg",
       name: "Signage",
       cost: 80,
       quantity: 0, 
